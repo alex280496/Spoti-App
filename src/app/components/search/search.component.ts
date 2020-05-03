a17a124810097026562d330cc5d5e91ece0ff3fa
Alex <omareyes-96@hotmail.com>
2020-05-03 14:06:32 -0500

    configuraiones de tokens de acceso para spotify

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styles: []
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
 
-  constructor() { }
+  artistas:any[]=[];
+  constructor(
+    private _spotify:SpotifyService
+  ) { }
 
-  ngOnInit() {
+  buscar(termino:string){
+    console.log(termino);
+    this._spotify.getArtista(termino).subscribe(
+      (data:any)=>{
+        console.log(data.artists.items);
+        this.artistas=data.artists.items;
+      }
+    );
   }
-
+  
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
-
+import {map} from 'rxjs/operators';
 @Injectable({
   providedIn: 'root' //para que ya no sea necesario importar el servicio en el app.module.ts
 })
@@ -11,8 +11,17 @@ export class SpotifyService {
   ) { }
   getNewReleases(){
     const headers=new HttpHeaders({
-      'Authorization':'Bearer BQD_TWaZDgY-SJ5_qBMfxIDA47luo8PRHRWCNKZfYo11pdeKaQ3g77UnC0siWrKunqaHXJaEWBOMvvCd1aw'
+      'Authorization':'Bearer BQAtE9VAed_iG7X_0iJEknvO_J-zydl1WfHS2vEtAy9d6rFQxMEdzAD_66nJ9dsTgmKoPyDMAqwaBjxtCs4'
     });
-    return this._http.get('https://api.spotify.com/v1/browse/new-releases?limit=20',{headers});
+    return this._http.get('https://api.spotify.com/v1/browse/new-releases?limit=20',{headers})
+      .pipe(map (data=>{
+        return data['albums'].items;
+      }));
+  }
+  getArtista(termino:string){
+    const headers=new HttpHeaders({
+      'Authorization':'Bearer BQAtE9VAed_iG7X_0iJEknvO_J-zydl1WfHS2vEtAy9d6rFQxMEdzAD_66nJ9dsTgmKoPyDMAqwaBjxtCs4'
+    });
+    return this._http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`,{headers});
   }
 }
